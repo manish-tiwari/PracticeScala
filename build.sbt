4a49b479953d2f83740ea441abdd9cbe777a86de
Manish Tiwari <P10417007@capita.co.in>
2017-08-31 18:40:42 +0530

    first commit which includes examples for:    * Helloworld    * HelloWorld with Main    * HelloWorld with arguments    * Helloworld with Logging with SBT initial code
    (cherry picked from commit 8ad6b5f008ed5d1cb424b5fd1e6dd7b140c69a95)

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "PracticeScala"
+
+version := "1.0"
+
+scalaVersion := "2.12.3"
+
+libraryDependencies ++= Seq("ch.qos.logback" % "logback-classic" % "1.1.7",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.4.0")
---
+++ src/main/scala/com/manish/practice/helloworldMain/com/manish/practice/helloworldWithArguments/HelloWorldWithArguments.scala
@@ -0,0 +1,10 @@
+package com.manish.practice.helloworldMain.com.manish.practice.helloworldWithArguments
+
+/**
+  * Created by P10417007 on 8/31/2017.
+  */
+object HelloWorldWithArguments extends App{
+
+  println("Hello World with Arguments Scala Application!")
+  println(args.mkString(", "))
+}
