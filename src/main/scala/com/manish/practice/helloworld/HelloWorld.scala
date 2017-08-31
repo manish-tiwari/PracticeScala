96c3629d69b6a23967c277b24ba6697b57de8fdb
Manish <P10417007@capita.co.in>
2017-08-31 18:47:33 +0530

    tqRevert "first commit which includes examples for:"
    This reverts commit 8ad6b5f008ed5d1cb424b5fd1e6dd7b140c69a95.

--- src/main/scala/com/manish/practice/helloworld/HelloWorld.scala
+++
@@ -1,8 +0,0 @@
-package com.manish.practice.helloworld
-
-/**
-  * Created by P10417007 on 8/31/2017.
-  */
-object HelloWorld extends App {
-  println("Hello World from Scala.")
-}
--- src/main/scala/com/manish/practice/sbtDependencies/HelloWordlWithScalaLogging.scala
+++
@@ -1,8 +0,0 @@
-package com.manish.practice.sbtDependencies
-
-/**
-  * Created by P10417007 on 8/31/2017.
-  */
-object HelloWordlWithScalaLogging extends App{
-
-}
