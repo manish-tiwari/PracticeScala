604ec3d2caaba575cb0537d8d8c762ae9a656651
Manish <manish-tiwari@github.com>
2017-09-01 16:14:34 +0530

    Logging with scala-logging (with errors)

--- src/main/scala/com/manish/practice/helloworldMain/com/manish/practice/helloworldWithArguments/HelloWorldWithArguments.scala
+++ src/main/scala/com/manish/practice/helloworldMain/com/manish/practice/helloworldWithArguments/HelloWorldWithArguments.scala
@@ -1,7 +1,7 @@
 package com.manish.practice.helloworldMain.com.manish.practice.helloworldWithArguments
 
 /**
-  * Created by P10417007 on 8/31/2017.
+  * Created by Manish on 8/31/2017.
   */
 object HelloWorldWithArguments extends App{
 
